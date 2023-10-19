462ffd24f3ac5b9f8916f2ff935a6743c1e4a859
yeyanghui <yeyanghui@lixiang.com>
2023-10-19 10:02:23 +0800

    chore: add emptyLayout

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Layout from '@/layout';
+import EmptyLayout from '@/layout/EmptyLayout';
 Vue.use(VueRouter);
 
 export const routes = [
@@ -30,27 +31,45 @@ export const routes = [
 		children: [
 			{
 				path: '/dashboard/table',
-				name: 'Table',
-				component: () => import('@/views/dashboard/index'),
+				name: '/dashboard/table',
+				redirect: '/dashboard/table/third1',
+				component: EmptyLayout,
 				meta: { title: '二级菜单一', icon: 'el-icon-data-analysis' },
 				children: [
 					{
-						path: '/dashboard/table',
-						name: 'Table',
-						component: () => import('@/views/dashboard/index'),
-						meta: { title: '三级菜单一', icon: 'el-icon-data-analysis' },
+						path: '/dashboard/table/third1',
+						name: '/dashboard/table/third1',
+						redirect: '/dashboard/table/third1/fourth1',
+						component: EmptyLayout,
+						meta: {
+							title: '三级菜单一',
+							icon: 'el-icon-data-analysis',
+						},
 						children: [
 							{
-								path: '/dashboard/table',
-								name: 'Table',
-								component: () => import('@/views/dashboard/index'),
-								meta: { title: '四级菜单一', icon: 'el-icon-data-analysis' },
+								path: '/dashboard/table/third1/fourth1',
+								name: '/dashboard/table/third1/fourth1',
+								component: () =>
+									import('@/views/dashboard/index'),
+								meta: {
+									title: '四级菜单一',
+									icon: 'el-icon-data-analysis',
+								},
+							},
+							{
+								path: '/dashboard/table/third1/fourth2',
+								name: '/dashboard/table/third1/fourth2',
+								component: () =>
+									import('@/views/dashboard/index2'),
+								meta: {
+									title: '四级菜单二',
+									icon: 'el-icon-data-analysis',
+								},
 							},
 						],
 					},
 				],
 			},
-			
 		],
 	},
 	{
@@ -64,18 +83,24 @@ export const routes = [
 				path: '/example/table',
 				name: 'Table',
 				component: () => import('@/views/example/table'),
-				meta: { title: '顶部菜单二/二级菜单一', icon: 'el-icon-data-analysis' },
+				meta: {
+					title: '顶部菜单二/二级菜单一',
+					icon: 'el-icon-data-analysis',
+				},
 			},
 			{
 				path: '/example/tree',
 				name: 'Tree',
 				component: () => import('@/views/example/tree'),
-				meta: { title: '顶部菜单二/二级菜单一', icon: 'el-icon-data-analysis' },
+				meta: {
+					title: '顶部菜单二/二级菜单一',
+					icon: 'el-icon-data-analysis',
+				},
 			},
 		],
 	},
 	// 404 要放在最后
-	{ path: '*', redirect: '/404', hidden: true }
+	{ path: '*', redirect: '/404', hidden: true },
 ];
 
 const router = new VueRouter({
